44f61bbc764d04c6495a3f15bd541bbe7142b5e5
Samantha Felix <166247516+SamyFelix@users.noreply.github.com>
2025-06-18 12:04:01 -0300

    Baixndo

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function diminuirTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 };
 
 function aumentarTamanho(){
@@ -18,4 +19,23 @@ function aumentarTamanho(){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 };
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKNMOPQRSTUVWXYZ';
+
+
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for(let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas [numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+
+
