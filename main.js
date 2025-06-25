cbf4ae4651933aa8d28cd79515c3a7836a5c3d84
Samantha Felix <166247516+SamyFelix@users.noreply.github.com>
2025-06-25 11:51:01 -0300

    Baixando

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function aumentarTamanho(){
 
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector ('.forca');
 
 for(i = 0; i < checkbox.length; i++){
     checkbox[i].onclick = geraSenha;
@@ -59,6 +60,20 @@ function geraSenha(){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificacaSenha();
+}
+
+function classificacaSenha(){
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if(tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');   
+    }
+    else if(tamanhoSenha > 5 && tamanhoSenha <12){
+        forcaSenha.classList.add('media');   
+    }
+    else if(tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');   
+    }
 }
 
 
